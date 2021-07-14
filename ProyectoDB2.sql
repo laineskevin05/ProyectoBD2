84e42d7c4927024956711b38d02e7a3bb970dd0c
Kevin Darel Laines <lainezkevin05@gmail.com>
2021-07-14 01:11:09 -0600

    Update ProyectoDB2.sql

--- ProyectoDB2.sql
+++ ProyectoDB2.sql
@@ -26,6 +26,7 @@ CREATE TABLE RH_Empleado(
 	FechaDeModificacion datetime NOT NULL CONSTRAINT DF_Empleado_FechaDeModificacion  DEFAULT (getdate()), 
 ) 
 GO
+
 --lleva el registro del historial de cambios de departamento y jornada de los empleados
 CREATE TABLE RH_HistorialEmpleadoDepartamento(
 	IdPersona nvarchar(15) NOT NULL, --llave foranea
@@ -35,12 +36,14 @@ CREATE TABLE RH_HistorialEmpleadoDepartamento(
 	FechaDeInicio date NOT NULL,
 	FechaDeFinalizacion date NULL,
 	FechaDeModificacion datetime NOT NULL CONSTRAINT DF_HistorialEmpleadoDepartamento_FechaDeModificacion  DEFAULT (getdate()),
-	primary key (IdPersona, IdDepartamento, IdJornada)
+	PRIMARY KEY(IdPersona, IdDepartamento, IdJornada)
 ) 
 GO
+
 --Cada dia es un nuevo turno.
 CREATE TABLE RH_Jornada(
 	IdJornada tinyint IDENTITY(1,1) NOT NULL,
+
 	Nombre varchar(15) NOT NULL,
 	HoraInicio time NOT NULL,
 	HoraSalida time NOT NULL,
@@ -51,7 +54,7 @@ GO
 CREATE TABLE RH_HistorialPagoEmpleado(
 	Id int primary key,
 
-	IdPersona nvarchar(15) NOT NULL,
+	IdPersona nvarchar(15) NOT NULL, --llave foranea
 	FechaDeCambioDePago datetime NOT NULL,
 	SalarioPoHora money NOT NULL,
 	FecuenciaDePago tinyint NOT NULL,
@@ -63,7 +66,7 @@ GO
 CREATE TABLE P_Direccion(
 	IdDireccion int IDENTITY(1,1) NOT NULL,
 
-	IdPersona nvarchar(15) NOT NULL, --foreng key
+	IdPersona nvarchar(15) NOT NULL, --llave foranea
 	DireccionDescripcion1 nvarchar(60) NOT NULL,
 	DireccionDescripcion2 nvarchar(60) NULL,
 	Ciudad nvarchar(30) NOT NULL,
@@ -81,7 +84,7 @@ CREATE TABLE P_Persona(
 	PrimerNombre nvarchar(20) NOT NULL,
 	SegundoNombre nvarchar(20) NULL,
 	PrimerApellido nvarchar(20) NOT NULL,
-	SegundoApellido nvarchar(20) NOT NULL,
+	SegundoApellido nvarchar(20) NULL,
 	InformacionAdicional nvarchar(60) NULL,
 	FechaDeModificacion datetime NOT NULL CONSTRAINT DF_Persona_FechaDeModificacion  DEFAULT (getdate()),
 )
@@ -90,7 +93,8 @@ GO
 --El telefono/s de la persona, ya sea cliente o empleado
 CREATE TABLE P_PersonaTelefono(
 	IdTelefono int IDENTITY(1,1) NOT NULL,
-	IdPersona nvarchar(15) NOT NULL, -- foreng key
+
+	IdPersona nvarchar(15) NOT NULL, --llave foranea
 	NumeroTelefono nvarchar(20) NOT NULL,
 	FechaDeModificacion datetime NOT NULL CONSTRAINT DF_PersonaTelefono_FechaDeModificacion  DEFAULT (getdate()),
 ) 
@@ -102,6 +106,7 @@ GO
 --En el inventario van los intrumentos o herramientas usadas en el hotel, por ejemplo cameras, herramientas para reparacion, etc.
 CREATE TABLE A_Inventario(
 	IdInventario int IDENTITY(1,1) NOT NULL,
+
 	Nombre nvarchar(30) NOT NULL,
 	cantidadExistencia smallint NOT NULL,
 	Tamanio nvarchar(12) NULL,
@@ -113,6 +118,7 @@ GO
 -- Productos que se venderan a los clientes
 CREATE TABLE A_Producto(
 	IdProducto int IDENTITY(1,1) NOT NULL,
+
 	Nombre nvarchar(30) NOT NULL,
 	Precio_venta money NOT NULL,	Descripcion nvarchar(40) NULL,	FechaDeModificacion datetime NOT NULL CONSTRAINT DF_Producto_FechaDeModificacion  DEFAULT (getdate()),)
 GO
@@ -122,8 +128,8 @@ CREATE TABLE A_OrdenCompra(
 	IdOrdenCompraInventario int IDENTITY(1,1) NOT NULL,
 
 	IdEmpleado int NOT NULL, --llave foranea
-	IdInventario int NULL,
-	IdProducto int NULL,
+	IdInventario int NULL, --llave foranea
+	IdProducto int NULL, --llave foranea
 	Cantidad smallint NOT NULL,
 	PrecioTotal money NOT NULL,
 	Descipcion nvarchar(40) NULL,
@@ -147,11 +153,37 @@ GO
 CREATE TABLE V_Reserva(
 	IdReserva int IDENTITY(1,1) NOT NULL,
 
-	IdHabitacion int NOT NULL,
-	IdPersona int NOT NULL,
-	IdEmpleado int NULL,
+	IdHabitacion int NOT NULL, --llave foranea
+	IdPersona int NOT NULL, --llave foranea
+	IdEmpleado int NULL, --llave foranea
 	Fecha_ingresa date NOT NULL,
-	Fecha_sale date NOT NULL,	FechaDeModificacion datetime NOT NULL CONSTRAINT DF_Reserva_FechaDeModificacion  DEFAULT (getdate()),
+	Fecha_sale date NOT NULL,	SubTotal money NULL,	Estado nvarchar NULL check (Estado IN('Activa','Cancelada')),	FechaDeModificacion datetime NOT NULL CONSTRAINT DF_Reserva_FechaDeModificacion  DEFAULT (getdate()),
 )
 GO
 
+--Cuando llega a recepcion y empieza a disfrutar del hospedaje
+CREATE TABLE V_Registro(
+	IdRegistro int IDENTITY(1,1) NOT NULL,
+
+	IdEmpleado int NULL, --llave foranea
+	Fecha_ingreso datetime NOT NULL,
+	Fecha_salida datetime NOT NULL,	SubTotal money NULL,	Decuento money NOT NULL CONSTRAINT DF_Registro_Descuento DEFAULT (0),	Total nvarchar NULL, 	FechaDeModificacion datetime NOT NULL CONSTRAINT DF_Reserva_FechaDeModificacion  DEFAULT (getdate()),
+)
+GO
+
+CREATE TABLE V_Consumo(
+	IdConsumo int IDENTITY(1,1) NOT NULL,
+
+	IdRegistro int NULL, --llave foranea
+	IdProducto int NOT NULL, --llave foranea
+	Cantidad smallint NOT NULL,
+	Precio_total money NOT NULL,	FechaDeModificacion datetime NOT NULL CONSTRAINT DF_Consumo_FechaDeModificacion  DEFAULT (getdate()),)GOCREATE TABLE V_Pago(
+	IdPago int IDENTITY(1,1) NOT NULL,
+
+	IdRegistro int NOT NULL, --llave foranea
+	Tipo_comprobante nvarchar check (Tipo_comprobante IN('Normal','Con RTN')),
+	Total_pago money NOT NULL,
+	Fecha_pago datetime CONSTRAINT DF_pago_FechaDePago DEFAULT (getdate()),
+	FechaDeModificacion datetime NOT NULL CONSTRAINT DF_Pago_FechaDeModificacion  DEFAULT (getdate())
+)
+GO
