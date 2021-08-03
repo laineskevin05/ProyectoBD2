05f6af118ea5ba0a970c6418985c00bfb6309eea
Kevin Darel Laines <lainezkevin05@gmail.com>
2021-08-03 01:12:43 -0600

    Update OLAP.sql

--- OLAP.sql
+++ OLAP.sql
@@ -5,3 +5,15 @@ CREATE DATABASE Hotel_OLAP;
 Go
 
 USE Hotel_OLAP;
+
+
+
+
+CREATE TABLE Clientes(
+	IdPersona nvarchar(15) PRIMARY KEY NOT NULL,
+
+	PrimerNombre nvarchar(20),
+	PrimerApellido nvarchar(20),
+	Tipo nvarchar(20)
+)
+GO
