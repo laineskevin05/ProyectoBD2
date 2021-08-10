fba7609b7baff85c39084ab62122ad8605399c9a
Kevin Darel Laines <lainezkevin05@gmail.com>
2021-08-10 15:04:05 -0600

    Herramienta actualizada

--- Herramienta/prueba.py
+++ Herramienta/prueba.py
@@ -1,9 +1,4 @@
-import random
-from datetime import datetime
-
-inicio = datetime(1980, 1, 1)
-final =  datetime(2001, 12, 28)
-
-random_date = inicio + (final - inicio) * random.random()
-
-print(random_date)
+#llenadoPersonas()
+#llenadoEmpleados()
+#llenadoHabitaciones()
+#llenadoProducto_E_Inventario()
